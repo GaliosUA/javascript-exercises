d7b181a58307a9e19c9d72eaeb0cde75c150dad3
Galios Elvensong <darkazarak@gmail.com>
2023-05-02 09:30:47 +0300

    Finish three exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string = "") {
+    let newString = '';
+    let stringToArray = string.split("");
+    for (let i = 0; i < string.length; i++) {
+    newString = newString.concat(stringToArray.pop());
+    }
+    return newString;
 };
-
+reverseString();
 // Do not edit below this line
 module.exports = reverseString;
