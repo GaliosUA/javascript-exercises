bcb3517e2d27eeec94c99734f22b485cb1397300
Galios Elvensong <darkazarak@gmail.com>
2023-05-02 14:22:12 +0300

    Finish four exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string = '', num) {
+    let text = '';
+    if (num < 0) return 'ERROR';
+    while (num > 0) {
+        text = text.concat(string);
+        num--;
+    }
+    return text;
+}
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,...args) {
+    return array.filter(value => !args.includes(value));
 };
 
+removeFromArray([1, 2, 3, 4], 3);
 // Do not edit below this line
 module.exports = removeFromArray;
