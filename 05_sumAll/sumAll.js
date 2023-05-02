3a9ddc68823c0c7cf521cf249d7a6a5be815c94d
Galios Elvensong <darkazarak@gmail.com>
2023-05-02 15:52:04 +0300

    Finish the fifth  exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function(intOne = 1, intTwo = 1) {
+    let iter = intTwo - intOne + 1;
+    let finalSum = 0;
+    if (intOne < 0 || intTwo < 0) {
+        return 'ERROR';
+    } 
+    else if (typeof intOne != 'number' || typeof intTwo != 'number') {
+        return 'ERROR';
+    } 
+    else if (intOne > intTwo) {
+        iter = intOne - intTwo + 1;
+        while (iter > 0){
+            finalSum += intTwo++;
+            iter--;
+        }
+    }
+    else {
+        while (iter > 0){
+            finalSum += intOne++;
+            iter--;
+        }
+    } 
+    return finalSum;
 };
-
+sumAll(1, 4);
 // Do not edit below this line
 module.exports = sumAll;
