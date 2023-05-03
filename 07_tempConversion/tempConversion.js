0ecb61a5d9412ab6e40abf6750acb2c3ef3af57c
Galios Elvensong <darkazarak@gmail.com>
2023-05-03 11:13:14 +0300

    Finish seven exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let convert = (temp - 32) * 5/9;
+  if (Number.isInteger(convert))
+    {
+      return +convert.toFixed();
+    }
+    else return +convert.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let convert = temp * 9/5 + 32;
+  if (Number.isInteger(convert))
+    {
+      return +convert.toFixed();
+    }
+    else return +convert.toFixed(1);
 };
 
+convertToCelsius();
+convertToFahrenheit();
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
