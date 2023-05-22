675626cfef9a12eb386687a16aab9c648894a3c3
Galios Elvensong <darkazarak@gmail.com>
2023-05-22 06:45:00 +0300

    Do the calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+	arr.forEach(item => sum += item);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let mul = 1;
+  arr.forEach(item => mul *= item);
+  return mul;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(a) {
+  let fact = 0;
+  if (a == 0 || a == 1) return 1;
+  for (let i = a - 1; i > 0; i--) {
+    fact = a * i;
+    a = fact;
+  }
+  return fact;
+}
 
 // Do not edit below this line
 module.exports = {
