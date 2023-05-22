025be0e8aa436f67a28aa66678d7ec5f6ec427e1
Galios Elvensong <darkazarak@gmail.com>
2023-05-22 10:30:49 +0300

    Do the fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 0) return 'OOPS';
+    const phi = 1.618033988;
+    return fibo = Math.round(Math.pow(phi, number) / Math.sqrt(5));
 };
 
 // Do not edit below this line
