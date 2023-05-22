f3dc077252aa01c40c753c8e0f459a0bcaa2f175
Galios Elvensong <darkazarak@gmail.com>
2023-05-22 09:59:52 +0300

    Do the palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.replaceAll(' ', '');
+    string = string.replaceAll('!', '');
+    string = string.replaceAll('.', '');
+    string = string.replaceAll(',', '');
+    let arr = [...string];
+    return arr.join('').toLowerCase() === arr.reverse().join('').toLowerCase();
 };
 
 // Do not edit below this line
