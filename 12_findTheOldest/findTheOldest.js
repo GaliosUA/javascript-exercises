51f74829b1cb3ec0ccc695b3dc16a38b7eb8136e
Galios Elvensong <darkazarak@gmail.com>
2023-05-23 07:23:02 +0300

    Do the find the oldest exercise but need to work on it

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  people.forEach(person => person.age = person.yearOfDeath - person.yearOfBirth);
+  people.sort(compare);
+  return people[0];
+};
 
+function compare(a, b) {
+  if (a.age < b.age) return 1; 
+  if (a.age == b.age) return 0; 
+  if (a.age > b.age) return -1;
+  if (isNaN(a.age)) {
+    a.age = 2023 - a.yearOfBirth;
+    if (a.age < b.age) return 1; 
+    if (a.age == b.age) return 0; 
+    if (a.age > b.age) return -1;
+  } 
+  if (isNaN(b.age)) {
+    b.age = 2023 - b.yearOfBirth;
+    if (a.age < b.age) return 1; 
+    if (a.age == b.age) return 0; 
+    if (a.age > b.age) return -1;
+  }
 };
 
 // Do not edit below this line
